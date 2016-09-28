39f6954119c9a95af9a049a193f282d2997adaf7
georgie3 <georgie3@hotmail.com>
2016-09-28 13:31:07 -0400

    Added 2 files

---
+++ VariablePractice.java
@@ -0,0 +1,22 @@
+
+public class VariablePractice {
+    
+    private int field1;
+ 
+    public static void main(String[] args) {
+        int wholeNum;
+        double bankBalance = 1000;
+        String empty = "";
+        String myAddress = "1 Yonge St";
+        long population = 30000000;
+        boolean decision = true;
+        char letter = 'G';
+        
+        wholeNum = 42;
+        
+        System.out.println("The value of wholeNum is " + wholeNum);
+        System.out.println("The decision is " + decision);
+        System.out.println("Your bank balance is $" + bankBalance);
+        System.out.println("My address is " + myAddress);
+    }
+}
---
+++ VariableTester.java
@@ -0,0 +1,22 @@
+
+public class VariableTester {
+    
+    public static void main(String[] args) {
+        int value;
+        value = 5;
+        value = 10;
+        String value2 = "John";
+        boolean isCorrect;
+        isCorrect = true;
+        isCorrect = false;
+        char ch;
+        ch = 'x';
+        ch = 'Z';
+        byte smallValue;
+        smallValue = 78;
+        smallValue = 123;
+        
+        VariableTester test = new VariableTester();
+        
+    }
+}
